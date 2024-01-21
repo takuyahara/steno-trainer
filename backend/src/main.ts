7580b618e3bbb2454e46bd9a76f60c9003436beb
takuyahara <h.taku86@gmail.com>
2024-01-21 19:18:40 +0900

    ko

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,25 +1,47 @@
 import { type Server } from "bun";
 import { find } from "./api";
 
-Bun.serve({
-  fetch(request: Request, server: Server) {
-    const url = new URL(request.url);
-    if (!url.pathname.startsWith("/q/")) {
-      return new Response("404", {
-        status: 404,
-      });
+const fetch = (request: Request, server: Server) => {
+  const url = new URL(request.url);
+  if (!url.pathname.startsWith("/q/")) {
+    return new Response("404", {
+      status: 404,
+    });
+  }
+  const query = url.pathname.substring(3);
+  const queryDecoded = decodeURIComponent(query);
+  console.log(queryDecoded);
+  const result = find(queryDecoded);
+  const json = JSON.stringify(result.slim());
+  console.log(json);
+  return new Response(json, {
+    headers: {
+      // "Access-Control-Allow-Origin": "http://localhost:5173",
+      "Content-Type": "application/json;charset=utf-8",
+    },
+  });
+};
+
+// Bun.serve({
+//   fetch,
+// });
+
+export default {
+  async fetch(request: Request, server: Server) {
+    let text = "Hello from Bun on Vercel!\n";
+
+    text += `\nurl: ${request.url}\n`;
+
+    for (const [key, value] of request.headers.entries()) {
+      if (!key.startsWith("x-vercel")) continue;
+      text += `\n${key}: ${value}`;
     }
-    const query = url.pathname.substring(3);
-    const queryDecoded = decodeURIComponent(query);
-    console.log(queryDecoded);
-    const result = find(queryDecoded);
-    const json = JSON.stringify(result.slim());
-    console.log(json);
-    return new Response(json, {
+
+    return new Response(text, {
+      status: 200,
       headers: {
-        // "Access-Control-Allow-Origin": "http://localhost:5173",
-        "Content-Type": "application/json;charset=utf-8",
+        "Content-Type": "text/plain;charset=utf-8",
       },
     });
   },
-});
+};
